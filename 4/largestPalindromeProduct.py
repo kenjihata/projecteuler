1a1f1d86c4394144c9e2c782c763fa406083d435
Kenji Hata <a.kenjihata@gmail.com>
2014-05-27 11:31:13 -0700

    finished 4, could make it more efficient

---
+++ 4/largestPalindromeProduct.py
@@ -0,0 +1,20 @@
+def largestPalindromeProduct():
+	maxNumber = 0
+	for i in xrange(999,100,-1):
+		for j in xrange(999, 100,-1):
+			if isPalindrome(i*j) and i*j > maxNumber:
+				maxNumber = i*j
+	return maxNumber
+
+def isPalindrome(n):
+	copy = n
+	reverse = 0
+	while (n > 0):
+		LSD = n%10
+		reverse = reverse*10 + LSD
+		n = (n-LSD)/10
+	return copy == reverse
+
+print largestPalindromeProduct()
+
+
