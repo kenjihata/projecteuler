58909c478e9893bf5fa1db6341c0d2479e0c1bdf
Kenji Hata <a.kenjihata@gmail.com>
2014-06-14 00:33:42 -0700

    problem 8 done

---
+++ 8/largestAdjacentProd.py
@@ -0,0 +1,17 @@
+import sys
+
+def adjacentProduct(string):
+	prod = 1
+	for i in xrange(0,len(string)):
+		prod *= int(string[i])
+	return prod
+
+inpStr = sys.argv[1]
+adjLen = int(sys.argv[2])
+
+maxProd = 0
+for i in xrange(0, len(inpStr) - adjLen):
+	prod = adjacentProduct(inpStr[i:i+adjLen])
+	if prod > maxProd:
+		maxProd = prod
+print maxProd
