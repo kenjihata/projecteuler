07898478db5366a352808f7406ee6757bf65072c
Kenji Hata <a.kenjihata@gmail.com>
2014-06-15 18:36:34 -0700

    fast prime sieve

--- 10/sumPrimesUnder.py
+++ 10/sumPrimesUnder.py
@@ -1,34 +1,14 @@
 import sys
 
-def isPrime(num, primelist):
-	for i in primelist:
-		if i > num/2:
-			return True
-		if num % i == 0:
-			return False
-	return True
+def generatePrimesUnder(upperBound):
+	primelist = []
+	isPrime = [True]*upperBound
+	isPrime[0] = False
+	isPrime[1] = False
+	for ind,val in enumerate(isPrime):
+		if val is True:
+			isPrime[ind*2::ind] = [False] *(((upperBound-1)//ind)-1)
+	return [i for i in range(len(isPrime)) if isPrime[i]]
 
-def generatePrimesUnder(upper):
-	if(upper < 2):
-		return []
-	primelist = [2]
-	for i in xrange(3, upper,2):
-		if isPrime(i, primelist):
-			primelist.append(i)
-	return primelist
-
-def generatePrimesUnder2(upper):
-	if upper<2:
-		return []
-	primelist = [2]
-	primelist.append(range(3, upper,2))
-	numPrimes = 1
-	while numPrimes < len(primelist):
-		sieveNum = primelist[numPrimes]
-		for i in primelist[numPrimes+1:]:
-			if i % sieveNum == 0:
-				primelist.remove(i)
-	return primelist
-
-primelist = generatePrimesUnder2(int(sys.argv[1]))
+primelist = generatePrimesUnder(int(sys.argv[1]))
 print sum(primelist)
--- 7/findPrime.py
+++ 7/findPrime.py
@@ -1,18 +1,16 @@
 import math
 
 def findNthPrime(n):
-	primelist = [2]
-	currNum = 3
-	for i in xrange(1,n):
-		while True:
-			isPrime = True
-			for i in primelist:
-				if currNum%i == 0:
-					isPrime = False
-			if isPrime:
-				primelist.append(currNum)
-				currNum += 1
-				break
-			currNum += 1
-	return primelist[-1]
-
+	logn = math.log(n)
+	upperBound = int(math.ceil(n*(logn + math.log(logn) - 0.9484)))
+	primelist = [True]*upperBound
+	primelist[0] = False
+	primelist[1] = False
+	numPrimes = 0
+	for ind,val in enumerate(primelist):
+		if val is True:
+			primelist[ind*2::ind] = [False] *(((upperBound-1)//ind)-1)
+			numPrimes += 1
+		if numPrimes == n:
+			return ind
+	return None
