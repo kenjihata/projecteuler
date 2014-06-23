39dc20512c5a23fdb2233fa1191b207973be682e
Kenji Hata <a.kenjihata@gmail.com>
2014-06-23 12:15:01 -0700

    problem 15 done

---
+++ 15/latticePath.py
@@ -0,0 +1,8 @@
+import sys
+import math
+
+def latticePathCount(gridLength):
+	if gridLength < 0: return None
+	return math.factorial(gridLength*2) / math.pow(math.factorial(gridLength),2)
+
+print latticePathCount(int(sys.argv[1]))
