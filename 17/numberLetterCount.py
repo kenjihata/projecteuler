9a0fe80b4b3b93110c45c28a87d44ea9ea3804e9
Kenji Hata <a.kenjihata@gmail.com>
2014-06-23 15:06:38 -0700

    problem 17 done, could be more efficient

---
+++ 17/numberLetterCount.py
@@ -0,0 +1,44 @@
+import sys
+
+countMap = {0:'', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety', 100:'hundred', 1000:'thousand'}
+
+def findDigitAt(num, digitPlace): 
+	return int(num/digitPlace) % 10
+
+# argument must be at most 3 digits long
+def findHundredsWord(num):
+	if num < 0 or num > 999: return None
+	if num <= 20:
+		return countMap[num]
+	elif num < 100: 
+		tens = findDigitAt(num,10)*10
+		onesDigit = findDigitAt(num,1)
+		return countMap[tens] + countMap[onesDigit]
+	else: 
+		hundredsDigit = findDigitAt(num,100)
+		tens = findDigitAt(num,10)*10
+		onesDigit = findDigitAt(num,1)
+		word = countMap[hundredsDigit] + countMap[100]
+		if tens+onesDigit == 0:
+			return word
+		else: word += 'and'
+		if tens+onesDigit <= 20:
+			return word + countMap[onesDigit+tens]
+		return word + countMap[tens] + countMap[onesDigit]
+	
+
+def letterCount(num):
+	if num < 1000:
+		word = findHundredsWord(num)
+		return len(word)
+	else:
+		return len(findHundredsWord(int(num/1000))+countMap[1000]+findHundredsWord(num%1000))
+		
+def numberLetterCount(lowerBound, upperBound):
+	if lowerBound < 0 or lowerBound > 999999 or upperBound < 0 or upperBound > 99999: return None
+	count = 0
+	for i in xrange(lowerBound, upperBound+1):
+		count += letterCount(i)
+	return count
+
+print(numberLetterCount(int(sys.argv[1]), int(sys.argv[2])))
